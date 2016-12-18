1b376e93a224b05c9297fa6d9b0f4ae184e270e3
hwani5 <hwaniiiii@gmail.com>
2016-12-18 10:24:42 +0900

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
+#    url(^post/(\d+)/$, include(admin.site.urls)),
 ]
